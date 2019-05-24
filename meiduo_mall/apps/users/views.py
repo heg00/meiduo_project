35809147881882ca6fcf0a88135e368b41b11ad8
xwp <xwp_fullstack@163.com>
2019-05-24 16:34:20 +0800

    注册后台功能完善

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,12 +1,14 @@
 import re
 
 from django import http
+from django.contrib.auth import login
 from django.db import DatabaseError
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 
 from apps.users.models import User
+from utils.response_code import RETCODE
 
 
 class RegisterView(View):
@@ -36,15 +38,32 @@ class RegisterView(View):
             return http.HttpResponseForbidden('请输入8-20位的密码')
         if password != password2:
             return http.HttpResponseForbidden('请输入8-20位的密码')
-        if not re.match(r'^[1[3-9]\d{9}$', mobile):
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
             return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 协议选中 前端会给个值 on
         if allow != 'on':
             return http.HttpResponseForbidden('请勾选用户协议')
 
         try:
-            User.objects.create_user(username=username, password=password, mobile=mobile)
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
         except DatabaseError:
-            return render(request, 'register.html', {'register_errmsg':'注册失败'})
-        #  namespace:name   主路由:子路由
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+        # django内部封装的login(),可以保持会话状态
+        # from django.contrib.auth import login
+        login(request, user)
+        #  namespace:name   主路由:子路由 注册成功，直接跳转到主页index
         return redirect(reverse('contents:index'))
 
+
+class UsernameCountView(View):
+    @staticmethod
+    def get(request, username):
+        count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
+
+class MobileCountView(View):
+    @staticmethod
+    def get(request, mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
